8aee27df10c7b764ad9e411793a2f6ae889133ab
Bhanuteja <shettyg964@gmail.com>
2026-03-10 18:22:12 +0530

    Initial commit - Smart Plot Login UI

---
+++ src/test/java/com/smartplot/smartplot/SmartplotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartplot.smartplot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartplotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
